9ed6d4bce1bd70aa641a612e159625b9e434edc4
chr15b <40733763+chr15b@users.noreply.github.com>
2025-12-02 22:46:15 +0000

    Step 1 and test associated updated

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,8 +9,10 @@
 
 // Add your code right below, good luck!
 
-
-
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
 
 // Do not change this
